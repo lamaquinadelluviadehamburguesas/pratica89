32d8f231f703d947f2b52905037a23dda3ff0b7f
joao <j1aj4v0er@gmail.com>
2026-03-25 22:40:53 -0600

    Autenticación con supabase E6

--- src/components/rutas/RutaProtegida.jsx
+++ src/components/rutas/RutaProtegida.jsx
@@ -1,4 +1,12 @@
+import { Navigate } from "react-router-dom";
+
 const RutaProtegida = ({ children }) => {
+  const usuarioGuardado = localStorage.getItem("usuario-supabase");
+
+  if (!usuarioGuardado) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <>
       {children}
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,15 +1,55 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import FormularioLogin from "../components/login/FormularioLogin";
+import { supabase } from "../database/supabaseconfig";
+import { Container } from "react-bootstrap";
 
 const Login = () => {
+  const [usuario, setUsuario] = useState("");
+  const [contrasena, setContrasena] = useState("");
+  const [error, setError] = useState(null);
+  const navegar = useNavigate();
+
+  useEffect(() => {
+    const usuarioGuardado = localStorage.getItem("usuario-supabase");
+    if (usuarioGuardado) {
+      navegar("/");
+    }
+  }, [navegar]);
+
+  const iniciarSesion = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: usuario,
+        password: contrasena,
+      });
+
+      if (error) {
+        setError("Usuario o contraseña incorrectos");
+        return;
+      }
+
+      if (data.user) {
+        localStorage.setItem("usuario-supabase", usuario);
+        navegar("/");
+      }
+    } catch (err) {
+      setError("Error al conectar con el servidor");
+      console.error("Error en la solicitud:", err);
+    }
+  };
+
   return (
-    <Container className="mt-3">
-      <Row className="align-items-center">
-        <Col>
-          <h2><i className="bi-person-fill-lock me-2"></i> Login</h2>
-        </Col>
-      </Row>
-    </Container>
+    <div className="estilo-contenedor-login">
+      <FormularioLogin
+        usuario={usuario}
+        contrasena={contrasena}
+        error={error}
+        setUsuario={setUsuario}
+        setContrasena={setContrasena}
+        iniciarSesion={iniciarSesion}
+      />
+    </div>
   );
 };
 
